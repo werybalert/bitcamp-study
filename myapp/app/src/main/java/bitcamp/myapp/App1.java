6bf22603a650b18c2511bd32ef306b58b36b97b9
werybalert <hooil0916@gmail.com>
2023-05-31 18:51:54 +0900

    0531

---
+++ myapp/app/src/main/java/bitcamp/myapp/App1.java
@@ -0,0 +1,336 @@
+package bitcamp.myapp;
+
+import java.util.Scanner;
+  
+public class App1 {
+    public static void main(String[] args) {
+     
+System.out.println("나의 목록 관리 시스템");
+System.out.println("----------------------------------");
+
+// 키보드 스캐너 준비
+Scanner scanner = new Scanner(System.in);
+
+// 1.
+System.out.print("번호? ");
+int no1 = scanner.nextInt();
+
+System.out.print("이름? ");
+String name1 = scanner.next();
+
+System.out.print("나이? ");
+int age1 = scanner.nextInt();
+
+System.out.print("재직중(true/false)? ");
+boolean working1 = scanner.nextBoolean();
+
+System.out.print("성별(남자:M, 여자:W)? ");
+String str = scanner.next();
+char gender1 = str.charAt(0);
+
+System.out.print("시력(왼쪽, 오른쪽)? ");
+float leftEye1 = scanner.nextFloat();
+float rightEye1 = scanner.nextFloat();
+
+
+// 2.
+System.out.print("번호? ");
+int no2 = scanner.nextInt();
+
+System.out.print("이름? ");
+String name2 = scanner.next();
+
+System.out.print("나이? ");
+int age2 = scanner.nextInt();
+
+System.out.print("재직중(true/false)? ");
+boolean working2 = scanner.nextBoolean();
+
+System.out.print("성별(남자:M, 여자:W)? ");
+str = scanner.next(); //기존변수 재사용
+char gender2 = str.charAt(0);
+
+System.out.print("시력(왼쪽, 오른쪽)? ");
+float leftEye2 = scanner.nextFloat();
+float rightEye2 = scanner.nextFloat();
+
+
+// 3.
+System.out.print("번호? ");
+int no3 = scanner.nextInt();
+
+System.out.print("이름? ");
+String name3 = scanner.next();
+
+System.out.print("나이? ");
+int age3 = scanner.nextInt();
+
+System.out.print("재직중(true/false)? ");
+boolean working3 = scanner.nextBoolean();
+
+System.out.print("성별(남자:M, 여자:W)? ");
+str = scanner.next();
+char gender3 = str.charAt(0);
+
+System.out.print("시력(왼쪽, 오른쪽)? ");
+float leftEye3 = scanner.nextFloat();
+float rightEye3 = scanner.nextFloat();
+
+
+// 4.
+System.out.print("번호? ");
+int no4 = scanner.nextInt();
+
+System.out.print("이름? ");
+String name4 = scanner.next();
+
+System.out.print("나이? ");
+int age4 = scanner.nextInt();
+
+System.out.print("재직중(true/false)? ");
+boolean working4 = scanner.nextBoolean();
+
+System.out.print("성별(남자:M, 여자:W)? ");
+str = scanner.next();
+char gender4 = str.charAt(0);
+
+System.out.print("시력(왼쪽, 오른쪽)? ");
+float leftEye4 = scanner.nextFloat();
+float rightEye4 = scanner.nextFloat();
+
+
+// 5.
+System.out.print("번호? ");
+int no5 = scanner.nextInt();
+
+System.out.print("이름? ");
+String name5 = scanner.next();
+
+System.out.print("나이? ");
+int age5 = scanner.nextInt();
+
+System.out.print("재직중(true/false)? ");
+boolean working5 = scanner.nextBoolean();
+
+System.out.print("성별(남자:M, 여자:W)? ");
+str = scanner.next(); //기존변수 재사용
+char gender5 = str.charAt(0);
+
+System.out.print("시력(왼쪽, 오른쪽)? ");
+float leftEye5 = scanner.nextFloat();
+float rightEye5 = scanner.nextFloat();
+
+
+// 6.
+System.out.print("번호? ");
+int no6 = scanner.nextInt();
+
+System.out.print("이름? ");
+String name6 = scanner.next();
+
+System.out.print("나이? ");
+int age6 = scanner.nextInt();
+
+System.out.print("재직중(true/false)? ");
+boolean working6 = scanner.nextBoolean();
+
+System.out.print("성별(남자:M, 여자:W)? ");
+str = scanner.next();
+char gender6 = str.charAt(0);
+
+System.out.print("시력(왼쪽, 오른쪽)? ");
+float leftEye6 = scanner.nextFloat();
+float rightEye6 = scanner.nextFloat();
+
+
+// 7.
+System.out.print("번호? ");
+int no7 = scanner.nextInt();
+
+System.out.print("이름? ");
+String name7 = scanner.next();
+
+System.out.print("나이? ");
+int age7 = scanner.nextInt();
+
+System.out.print("재직중(true/false)? ");
+boolean working7 = scanner.nextBoolean();
+
+System.out.print("성별(남자:M, 여자:W)? ");
+str = scanner.next();
+char gender7 = str.charAt(0);
+
+System.out.print("시력(왼쪽, 오른쪽)? ");
+float leftEye7 = scanner.nextFloat();
+float rightEye7 = scanner.nextFloat();
+
+
+// 8.
+System.out.print("번호? ");
+int no8 = scanner.nextInt();
+
+System.out.print("이름? ");
+String name8 = scanner.next();
+
+System.out.print("나이? ");
+int age8 = scanner.nextInt();
+
+System.out.print("재직중(true/false)? ");
+boolean working8 = scanner.nextBoolean();
+
+System.out.print("성별(남자:M, 여자:W)? ");
+str = scanner.next(); //기존변수 재사용
+char gender8 = str.charAt(0);
+
+System.out.print("시력(왼쪽, 오른쪽)? ");
+float leftEye8 = scanner.nextFloat();
+float rightEye8 = scanner.nextFloat();
+
+
+// 9.
+System.out.print("번호? ");
+int no9 = scanner.nextInt();
+
+System.out.print("이름? ");
+String name9 = scanner.next();
+
+System.out.print("나이? ");
+int age9 = scanner.nextInt();
+
+System.out.print("재직중(true/false)? ");
+boolean working9 = scanner.nextBoolean();
+
+System.out.print("성별(남자:M, 여자:W)? ");
+str = scanner.next();
+char gender9 = str.charAt(0);
+
+System.out.print("시력(왼쪽, 오른쪽)? ");
+float leftEye9 = scanner.nextFloat();
+float rightEye9 = scanner.nextFloat();
+
+
+// 10.
+System.out.print("번호? ");
+int no10 = scanner.nextInt();
+
+System.out.print("이름? ");
+String name10 = scanner.next();
+
+System.out.print("나이? ");
+int age10 = scanner.nextInt();
+
+System.out.print("재직중(true/false)? ");
+boolean working10 = scanner.nextBoolean();
+
+System.out.print("성별(남자:M, 여자:W)? ");
+str = scanner.next();
+char gender10 = str.charAt(0);
+
+System.out.print("시력(왼쪽, 오른쪽)? ");
+float leftEye10 = scanner.nextFloat();
+float rightEye10 = scanner.nextFloat();
+
+System.out.println("---------------------------------------");
+
+System.out.printf("번호: %d\n", no1);
+System.out.printf("이름: %s\n", name1);
+System.out.printf("나이: %d\n", age1);
+System.out.printf("재직자: %b\n", working1);
+System.out.printf("성별(남자(M), 여자(W)): %c\n", gender1);
+System.out.printf("좌우시력: %.1f,%.1f\n", leftEye1, rightEye1);
+
+
+System.out.printf("번호: %d\n", no2);
+System.out.printf("이름: %s\n", name2);
+System.out.printf("나이: %d\n", age2);
+System.out.printf("재직자: %b\n", working2);
+System.out.printf("성별(남자(M), 여자(W)): %c\n", gender2);
+System.out.printf("좌우시력: %.1f,%.1f\n", leftEye2, rightEye2);
+
+
+System.out.printf("번호: %d\n", no3);
+System.out.printf("이름: %s\n", name3);
+System.out.printf("나이: %d\n", age3);
+System.out.printf("재직자: %b\n", working3);
+System.out.printf("성별(남자(M), 여자(W)): %c\n", gender3);
+System.out.printf("좌우시력: %.1f,%.1f\n", leftEye3, rightEye3);
+
+
+System.out.printf("번호: %d\n", no4);
+System.out.printf("이름: %s\n", name4);
+System.out.printf("나이: %d\n", age4);
+System.out.printf("재직자: %b\n", working4);
+System.out.printf("성별(남자(M), 여자(W)): %c\n", gender4);
+System.out.printf("좌우시력: %.1f,%.1f\n", leftEye4, rightEye4);
+
+
+System.out.printf("번호: %d\n", no5);
+System.out.printf("이름: %s\n", name5);
+System.out.printf("나이: %d\n", age5);
+System.out.printf("재직자: %b\n", working5);
+System.out.printf("성별(남자(M), 여자(W)): %c\n", gender5);
+System.out.printf("좌우시력: %.1f,%.1f\n", leftEye5, rightEye5);
+
+
+System.out.printf("번호: %d\n", no6);
+System.out.printf("이름: %s\n", name6);
+System.out.printf("나이: %d\n", age6);
+System.out.printf("재직자: %b\n", working6);
+System.out.printf("성별(남자(M), 여자(W)): %c\n", gender6);
+System.out.printf("좌우시력: %.1f,%.1f\n", leftEye6, rightEye6);
+
+
+System.out.printf("번호: %d\n", no7);
+System.out.printf("이름: %s\n", name7);
+System.out.printf("나이: %d\n", age7);
+System.out.printf("재직자: %b\n", working7);
+System.out.printf("성별(남자(M), 여자(W)): %c\n", gender7);
+System.out.printf("좌우시력: %.1f,%.1f\n", leftEye7, rightEye7);
+
+
+System.out.printf("번호: %d\n", no8);
+System.out.printf("이름: %s\n", name8);
+System.out.printf("나이: %d\n", age8);
+System.out.printf("재직자: %b\n", working8);
+System.out.printf("성별(남자(M), 여자(W)): %c\n", gender8);
+System.out.printf("좌우시력: %.1f,%.1f\n", leftEye8, rightEye8);
+
+
+
+System.out.printf("번호: %d\n", no9);
+System.out.printf("이름: %s\n", name9);
+System.out.printf("나이: %d\n", age9);
+System.out.printf("재직자: %b\n", working9);
+System.out.printf("성별(남자(M), 여자(W)): %c\n", gender9);
+System.out.printf("좌우시력: %.1f,%.1f\n", leftEye8, rightEye9);
+
+
+
+System.out.printf("번호: %d\n", no10);
+System.out.printf("이름: %s\n", name10);
+System.out.printf("나이: %d\n", age10);
+System.out.printf("재직자: %b\n", working10);
+System.out.printf("성별(남자(M), 여자(W)): %c\n", gender10);
+System.out.printf("좌우시력: %.1f,%.1f\n", leftEye10, rightEye10);
+
+//10명 입력값 넣기
+
+
+scanner.close();
+      
+      // 출력
+      // 번호: 123
+      // 이름: 홍길동
+      // 나이: 23
+      // 재직자: true
+      // 성별(남자(M), 여자(W)): w
+      // 좌우시력: 1.5,1.0
+  
+  
+      // - 키보드로 값을 입력 받는 방법
+  
+    }
+  }
+  
+
+}
