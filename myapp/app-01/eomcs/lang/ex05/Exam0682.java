fd8c53ff9a57ebc46d17f0a0cf03b44d60760ba0
werybalert <hooil0916@gmail.com>
2023-06-01 16:27:50 +0900

    0601

--- myapp/app-01/eomcs/lang/ex05/Exam0682.java
+++ myapp/app-01/eomcs/lang/ex05/Exam0682.java
@@ -5,8 +5,7 @@ package com.eomcs.lang.ex05;
 public class Exam0682 {
   public static void main(String[] args) {
     // 주의!
-    // 1) pre-fix 연산자나 post-fix 연산자를 리터럴에 적용할 수 없다.
-    //    int x = ++100; // 컴파일 오류!
+    // 1) pre-fix 연산자나 post-fix 연산자를 리터럴에 적용할 수 없
     //    x = 100++; // 컴파일 오류!
 
     // 2) 변수에 동시에 적용할 수 없다.
---
+++ myapp/app05/App2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class App2 {
+    public static void main(String[] args) {
+        System.out.println("나의 목록 관리 시스템");
+
+        Scanner scanner = ner Scanner(System.in);
+
+        
+
+    }
+
+
+}
+
+
