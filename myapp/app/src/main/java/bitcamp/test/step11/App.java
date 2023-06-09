d9d7c8d4ca286e23e09c51927fd8f7096c853cba
werybalert <hooil0916@gmail.com>
2023-06-09 16:16:27 +0900

    Backup

---
+++ myapp/app/src/main/java/bitcamp/test/step11/App.java
@@ -0,0 +1,73 @@
+package bitcamp.test.step11;
+
+// 1) 낱개의 변수 사용
+// 2) 낱개의 변수 재사용
+// 3) 배열 사용
+// 4) 클래스를 이용하여 데이터 타입 정의(중첩클래스; 로컬 클래스)
+// 5) 출력 기능을 별도의 메서드로 분리(중첩클래스; 스태틱 중첩 클래스) 
+// 6) 합계 및 평균을 계산하는 기능을 메서드로 분리
+// 7) GRASP 패턴: Information Expert(정보를 갖고 있는 클래스가 그 정보를 다룬다.)
+// 8) 인스턴스 메서드 도입
+// 9) 객체 생성이 번거롭고 복잡한 경우 메서드로 분리하는 것이 낫다.(디자인패턴; 팩토리 메서드)
+// 10) GRASP 패턴: Information Expert
+// 11) 생성자 도입 : 인스턴스 변수를 보다 쉽게 초기화 시키기
+public class App {
+  
+  static class Score {
+    String name;
+    int kor;
+    int eng;
+    int math;
+    int sum;
+    float aver;
+
+    //생성자 : 인스턴스를 생성한 직후 호출하는 메서드
+
+    Score(String name, int kor, int eng, int math) {
+      this.name = name;
+      this.kor = kor;
+      this.eng = eng;
+      this.math = math;
+      this.compute();
+    }
+
+    void compute() {
+      this.sum = this.kor + this.eng + this.math;
+      this.aver = this.sum / 3f;
+    }
+
+    static Score create(String name, int kor, int eng, int math) {
+      Score s = new Score();
+      s.name = name;
+      s.kor = kor;
+      s.eng = eng;
+      s.math = math;
+      s.compute();
+      return s;
+    }
+  }
+  
+  public static void main(String[] args) {
+
+    final int MAX_SIZE = 10;
+    Score[] scores = new Score[MAX_SIZE];
+    int length = 0;
+
+    //new Score = Score 설계도에 따라 인스턴스 생성, 생성한 후 String, int, 파라미터 값을 받는 생성자 호출, 이렇게 초기화시킨 인스턴스 주소 리턴
+
+    scores[length++] = new Score("홍길동", 100, 100, 100);
+    scores[length++] = new Score("임꺽정", 90, 90, 90);
+    scores[length++] = Score.create("유관순", 80, 80, 80);
+
+    for (int i = 0; i < length; i++) {
+      printScore(scores[i]);
+    }
+
+  }
+
+  static void printScore(Score s) {
+    System.out.printf("%s: 합계=%d, 평균=%.1f\n", 
+      s.name, s.sum, s.aver);
+  }
+
+}
