d6b748967efe7c78c98861fd24d39782701d8e6f
werybalert <hooil0916@gmail.com>
2023-06-13 19:23:21 +0900

    0613

--- myapp/app/src/main/java/bitcamp/myapp2/App.java
+++ myapp/app/src/main/java/bitcamp/myapp2/App.java
@@ -1,7 +1,11 @@
 package bitcamp.myapp2;
 
-import bitcamp.myapp.handler.MemberHandler;
+import bitcamp.myapp2.handler.BoardHandler;
+import bitcamp.myapp2.handler.MemberHandler;
 import bitcamp.util.Prompt;
+
+
+// ctrl + shift + o
 
 public class App {
 
@@ -13,7 +17,7 @@ public class App {
 
     while (true) {
       String menuNo = Prompt.inputString("메인> ");
-      if (menuNo.equals("6")) {
+      if (menuNo.equals("99")) {
         break;
       } else if (menuNo.equals("menu")) {
         printMenu();
@@ -27,6 +31,17 @@ public class App {
         MemberHandler.updateMember();
       } else if (menuNo.equals("5")) {
         MemberHandler.deleteMember();
+        // 게시판 출력 번호 (06.13)
+      } else if (menuNo.equals("6")) {
+        BoardHandler.inputBoard();
+      } else if (menuNo.equals("7")) {
+        BoardHandler.printBoards();
+      } else if (menuNo.equals("8")) {
+        BoardHandler.viewBoard();
+      } else if (menuNo.equals("9")) {
+        BoardHandler.updateBoard();
+      } else if (menuNo.equals("10")) {
+        BoardHandler.deleteBoard();
       } else {
         System.out.println(menuNo);
       }
@@ -41,7 +56,14 @@ public class App {
     System.out.println("3. 회원조회");
     System.out.println("4. 회원변경");
     System.out.println("5. 회원삭제");
-    System.out.println("6. 종료");
+
+    // 0613 실습
+    System.out.println("6. 게시글등록");
+    System.out.println("7. 게시글목록");
+    System.out.println("8. 게시글조회");
+    System.out.println("9. 게시글변경");
+    System.out.println("10. 게시글삭제");
+    System.out.println("99. 종료");
   }
 
   static void printTitle() {
